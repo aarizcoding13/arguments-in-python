1bbaf119a8c9a283de01558b3e027097ac5ce6d3
aarizcoding13 <aarizhassankh@gmail.com>
2026-05-16 13:48:07 +0200

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,11 @@
+def shutdown():
+    user = input("Enter Yes or No: ")
+
+    if user == "Yes":
+        print("shuttingg down")
+    elif user == "No":
+        print("abort shut down")
+    else:
+        print("sorryy")
+
+shutdown()
